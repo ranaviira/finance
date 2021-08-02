2a900a7df8476aadf76ea8faae4060a99364838c
ranaviira <84rent@gmail.com>
2021-08-02 19:49:21 +0400

    add class Converter

--- src/finance/finHelp.java
+++ src/finance/finHelp.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 
 public class finHelp {
     public static void main(String[] args) {
-
         double[] expenses = new double[7];
+
         Scanner scanner = new Scanner(System.in);
+        Converter converter = new Converter(78.5, 88.7, 0.75);
 
         System.out.println("Сколько денег у вас осталось до зарплаты?");
         double moneyBeforeSalary = scanner.nextDouble();
@@ -14,44 +15,52 @@ public class finHelp {
         int daysBeforeSalary = scanner.nextInt();
 
         while (true) {
-
             printMenu();
             int command = scanner.nextInt();
 
-            if (command == 1) {convert(scanner, moneyBeforeSalary);}
-            else if (command == 2) {getAdvice(moneyBeforeSalary, daysBeforeSalary);}
-            else if (command == 3) {moneyBeforeSalary = saveExpense(scanner, moneyBeforeSalary, expenses);}
-            else if (command == 4) {printAllExpenses(expenses);}
-            else if (command == 5) {System.out.println("Самая большая сумма расходов на этой неделе составила " + findMaxExpense(expenses) + " руб.");}
-            else if (command == 0) {System.out.println("Выход"); break;}
-            else {System.out.println("Извините, такой команды пока нет.");}
+            if (command == 1) {
+                System.out.println("Ваши сбережения: " + moneyBeforeSalary + " RUB");
+                System.out.println("В какую валюту хотите конвертировать? Доступные варианты: 1 - USD, 2 - EUR, 3 - JPY.");
+                int currency = scanner.nextInt();
+                converter.convert(moneyBeforeSalary, currency);
+            } else if (command == 2) {
+                getAdvice(moneyBeforeSalary, daysBeforeSalary);
+            } else if (command == 3) {
+                moneyBeforeSalary = saveExpense(scanner, moneyBeforeSalary, expenses);
+            } else if (command == 4) {
+                printAllExpenses(expenses);
+            } else if (command == 5) {
+                System.out.println("Самая большая сумма расходов на этой неделе составила " + findMaxExpense(expenses) + " руб.");
+            } else if (command == 0) {
+                System.out.println("Выход");
+                break;
+            } else {
+                System.out.println("Извините, такой команды пока нет.");
+            }
         }
     }
 
-    public static double saveExpense(Scanner scanner, double moneyBeforeSalary, double[] expenses){
+    public static double saveExpense(Scanner scanner, double moneyBeforeSalary, double[] expenses) {
         System.out.println("За какой день вы хотите ввести трату: 1-ПН, 2-ВТ, 3-СР, 4-ЧТ, 5-ПТ, 6-СБ, 7-ВС?");
-
         int day = scanner.nextInt();
         System.out.println("Введите размер траты:");
         double expense = scanner.nextDouble();
         moneyBeforeSalary = moneyBeforeSalary - expense;
         expenses[day - 1] = expenses[day - 1] + expense;
-
+        System.out.println("Значение сохранено! Ваш текущий баланс в рублях: " + moneyBeforeSalary);
         if (moneyBeforeSalary < 1000) {
             System.out.println("На вашем счету осталось совсем немного. Стоит начать экономить!");
-
-
         }
         return moneyBeforeSalary;
     }
 
-    public static void printAllExpenses(double[] expenses){
+    public static void printAllExpenses(double[] expenses) {
         for (int i = 0; i < expenses.length; i++) {
             System.out.println("День " + (i + 1) + ". Потрачено " + expenses[i] + " рублей");
         }
-
     }
-    public static double findMaxExpense(double[] expenses){
+
+    public static double findMaxExpense(double[] expenses) {
         double maxExpense = 0;
         for (int i = 0; i < expenses.length; i++) {
             if (expenses[i] > maxExpense) {
@@ -74,7 +83,7 @@ public class finHelp {
     public static void getAdvice(double moneyBeforeSalary, int daysBeforeSalary) {
         if (moneyBeforeSalary < 3000) {
             System.out.println("Сегодня лучше поесть дома. Экономьте, и вы дотянете до зарплаты!");
-        } else if (moneyBeforeSalary < 10000){
+        } else if (moneyBeforeSalary < 10000) {
             if (daysBeforeSalary < 10) {
                 System.out.println("Окей, пора в Макдак!");
             } else {
@@ -94,25 +103,5 @@ public class finHelp {
             }
         }
     }
-
-    public static void convert(Scanner scanner, double moneyBeforeSalary) {
-        double rateUSD = 78.5;
-        double rateEUR = 85;
-        double rateJPY = 0.74;
-
-        System.out.println("Ваши сбережения: " + moneyBeforeSalary + " RUB");
-        System.out.println("В какую валюту хотите конвертировать? Доступные варианты: 1 - USD, 2 - EUR, 3 - JPY.");
-        int currency = scanner.nextInt();
-        if (currency == 1) {
-            System.out.println("Ваши сбережения в долларах: " + moneyBeforeSalary / rateUSD);
-        } else if (currency == 2) {
-            System.out.println("Ваши сбережения в евро: " + moneyBeforeSalary / rateEUR);
-        } else if (currency == 3) {
-            System.out.println("Ваши сбережения в иенах: " + moneyBeforeSalary / rateJPY);
-        } else {
-            System.out.println("Неизвестная валюта");
-        }
-    }
-
 }
 
